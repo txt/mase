b71905e52e2d54aa5eb04747ab99e912e497b8de
Tim Menzies <tim@menzies.us>
2015-04-25 15:29:25 -0400

    saving

---
+++ etc/toc.sh
@@ -0,0 +1,13 @@
+echo ""
+echo "# Table of Contents"
+echo ""
+files=`ls *.py | grep -v "ok.py"`
+for f in $files; do
+   awk ' gsub(/^# /,"") {
+         name=$0
+	 file=FILENAME
+         gsub(/.py$/,".md",file)
+         print "+ ["$0"](__PRE/doc/"file")"; 
+         exit}' $f
+done
+echo ""   
