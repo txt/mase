a6ecc6bf857c71ca5eb7ef8a255b63bdc94ca71c
Tim Menzies <tim.menzies@gmail.com>
2015-08-12 11:19:48 -0400

    saving

--- src/abstract.py
+++ src/abstract.py
@@ -1,6 +1,10 @@
 
 """
 
+[[ http://www.sheawong.com/wp-content/uploads/2013/08/keephatin.gif | height = 100px align = right ]]
+
+[[ http://www.sheawong.com/wp-content/uploads/2013/08/keephatin.gif | height = 100px | align = right ]]
+
 # Abstraction (Advanced Python Coding)
 
 Layers within layers within layers..
